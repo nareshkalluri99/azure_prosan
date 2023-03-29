4e411c7600a6e938f6ad75edecb3cc784ae44efb
venkat.satish <venkat.satish@prosanusa.com>
2023-03-29 17:10:57 -0400

    updated storage account details

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   
   backend "azurerm" {
-      resource_group_name  = "tfstate-rg"
-      storage_account_name = "tfstateprosan"
+      resource_group_name  = "storage-acct-rg"
+      storage_account_name = "prosanstrag1"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
