2480fd5c28723a3b9ebc8f002681007ffd9dda27
venkat.satish <venkat.satish@prosanusa.com>
2023-03-29 16:58:41 -0400

    working version v0 terraform code

--- backend.tf
+++ backend.tf
@@ -1,10 +1,5 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
+  
   backend "azurerm" {
       resource_group_name  = "tfstate-rg"
       storage_account_name = "tfstateprosan"
