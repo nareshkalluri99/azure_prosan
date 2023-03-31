6d1b6c1ef5eb815b5fee3dcd13789f8861561ee2
kodalisaikrishna12 <kodalisaikrishna12@gmail.com>
2023-03-31 15:12:32 -0400

    terraform

---
+++ terraform/terraform/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  
+  backend "azurerm" {
+      resource_group_name  = "tfstate-rg"
+      storage_account_name = "tfstateprosan"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "490ddf89-c649-48f6-a601-c54e6d8beb3e"
+  tenant_id       = "490ddf89-c649-48f6-a601-c54e6d8beb3e"
+}
+
+#resource "azurerm_resource_group" "state-demo-secure" {
+#  name     = "state-demo"
+#  location = "eastus"
+#}
