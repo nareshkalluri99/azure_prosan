cdf19737a0eb15c69006ec6140f88aa883b9c604
alekhya010893 <alekhyacloudops93@gmail.com>
2023-04-05 15:59:14 -0400

    projects upload

---
+++ java/prosan/src/main/java/com/prosan/demo.java
@@ -0,0 +1,12 @@
+package java.prosan.src.main.java.com.prosan;
+
+import java.io.*;
+public class demo {
+	public static void main(String args[]) throws InterruptedException {
+		for (int i=0;i<100;i++) {
+			System.out.println(i);
+			Thread.sleep(100);
+		}
+	}
+}
+
---
+++ learnjava/hw2.java
@@ -0,0 +1,13 @@
+import java.io.*
+
+/*
+ * 
+ * equivalent to #include<stdio.h>
+ */
+//save as FirstProgram.java  
+package learnjava;  
+public class hw2{  
+    public static void main(String args[]) {  
+        System.out.println("Welcome to package example");  
+    }  
+}
