fb97e39d599e941b45c64c938341b0c1c0ba2f2e
venkat.satish <venkat.satish@prosanusa.com>
2023-04-18 12:05:44 -0400

    functions basics

---
+++ funct_basics.py
@@ -0,0 +1,19 @@
+import math
+def rectangle(l, b, h):
+    area = l*b*h
+    return area
+
+def area_circle(r):
+    return math.pi*(r**2)
+
+def diameter_circle(r):
+    diameter = 2*math.pi*r
+    print(diameter)
+
+#find slope
+#area of square
+#find root of a quadratic equation and  x = (-b ± √ (b2 - 4ac)
+
+print(rectangle(4,4,5))
+#print(area_circle(10))
+#print(diameter_circle(9))
